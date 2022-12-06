7a4c15301b07e3d1d21183793368ae055c68ff47
SDDRom <enwamba65@gmail.com>
2022-12-06 11:13:07 +0100

    created a index.html

---
+++ test1.js
@@ -0,0 +1,9 @@
+// Section 2. Practice
+
+// 3) A function that sum ellements of an array
+ const ourArray = [2, 7, 3, 6, 4]; 
+    let sum = 0;
+    for (let i = 0; i < ourArray. length; i++) { 
+        sum = sum + ourArray[i];
+}
+console.log('The sum of the array ', sum);
